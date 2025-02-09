ca27c249b94196e396b71d070de11cbd12b8c30f
WhyK <windchimeyk@gmail.com>
2025-02-09 22:40:54 +0900

    feat(green): implemented CsvExtractor#readwiseToJson

--- core.ts
+++ core.ts
@@ -1,3 +1,5 @@
+import { parse } from "@std/csv";
+
 interface ReadwiseCsvToJsonOption {
   csv: string;
   ignoreFeed?: boolean;
@@ -9,8 +11,36 @@ export class CsvExtractor {
    * Readwise export CSV to JSON
    */
   public readwiseToJson(
-    { csv: _csv, ignoreFeed: _ignoreFeed = false }: ReadwiseCsvToJsonOption,
-  ) {
-    throw new Error("Not implemented");
+    { csv, ignoreFeed = false }: ReadwiseCsvToJsonOption,
+  ): Record<string, string | string[]>[] {
+    const data = parse(csv, { skipFirstRow: true });
+
+    return this.convertKeyValue(data, ignoreFeed);
+  }
+
+  private convertKeyValue(
+    data: Record<string, string>[],
+    ignoreFeed: boolean,
+  ): Record<string, string | string[]>[] {
+    const convertedData = data.map((arr) =>
+      Object.fromEntries(
+        Object.entries(arr).map((
+          [key, value],
+        ) => [
+          key.toLowerCase().replaceAll(" ", "_"),
+          /\[(.+)\]/.test(value) ? this.convertStringToArray(value) : value,
+        ]),
+      )
+    );
+
+    if (ignoreFeed) return convertedData.filter((d) => d.location !== "feed");
+
+    return convertedData;
+  }
+
+  private convertStringToArray(str: string): string[] {
+    return str.replace(/^\[|\]$/g, "").split(/,\s*/).map((s) =>
+      s.replace(/^'|'$/g, "")
+    );
   }
 }
